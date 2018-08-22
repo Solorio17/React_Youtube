a20dbcf7eabbde1dc9c9a3387fe8a5a896267170
Jason <jasonsoloriom@gmail.com>
2018-08-21 22:35:44 -0600

    adding the video player which is going to play the video, a conditional was placed on it to check if a video has been retrieved. The iframe has a const assigned to it which is going to have the videos url, which is used by getting its ID

--- src/components/video_player.js
+++ src/components/video_player.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
 const VideoPlayer = ({video}) => {
+  if(!video){
+    return <div>Loading...</div>
+  }
+  const videoId = video.id.videoId;
+  const url = `https://www.youtube.com/embed/${videoId}`;
   return (
     <div className="video-player col-md-8">
       <div className="embed-responsive embed-responsive-16by9">
-        <iframe className="embed-responsive-item"></iframe>
+        <iframe className="embed-responsive-item" src={url}></iframe>
       </div>
       <div className="details">
         <div>{video.snippet.title}</div>
